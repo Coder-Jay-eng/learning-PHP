d1be421cb240fcc31d346e25314c4275f7b18082
Aw3som3ss <jabwunza@gmail.com>
2024-01-10 20:51:24 +0300

    Data Types in php Variables

--- variables.php
+++ variables.php
@@ -14,6 +14,22 @@ echo $y;
 // How to refer to a variable in an output
 $txt = "W3Schools.com";
 echo "I love " . $txt . "!";
+
+
+#Data Types in Variables
+$x = 5;      // $x is an integer
+$y = "John"; // $y is a string
+$d = True; //Boolean
+
+/*
+php also accepts:
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+Resource
+*/
 ?>
 
 </body>
