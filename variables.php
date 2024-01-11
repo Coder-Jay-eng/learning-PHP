642c0a1121b8eb2a22356eae851717423eb5da67
Aw3som3ss <jabwunza@gmail.com>
2024-01-11 20:23:43 +0300

    Global Scope

--- variables.php
+++ variables.php
@@ -46,5 +46,29 @@ var_dump(true);
 var_dump([2, 3, 56]);
 var_dump(NULL);
 
+// Variable scope
+/*
+PHP has three types of variable scopes:
+local
+global
+static
+*/
+
+// Local scope
+// A variable declared outside a function has a GLOBAL SCOPE and can only be accessed outside a function:
+
+$x = 5; // global scope
+
+function myTest() {
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: $x</p>";
+}
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
+
+
+
+?>
 </body>
 </html>
