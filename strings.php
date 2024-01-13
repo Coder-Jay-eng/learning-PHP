0260e5db6f8e04df3a2d591b5d33646eb6fd7715
Aw3som3ss <jabwunza@gmail.com>
2024-01-13 14:49:50 +0300

    Single and double quotes in php strings

---
+++ strings.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+//Strings in php can be enclosed in single or double quotation marks.
+ $str1 = "My php string";
+ $str2 = 'my php string';
+
+ //Double quote performs action on special characters
+
+echo "This is $str1";   // Will print out This is My php string
+
+//Single quotes print the string as is:
+
+echo 'This is $str2'   // Will print out This is $str2
+
+?>
+</body>
+</html>
