42913ae79666093d6f19cd26398b1e84f81072cd
Aw3som3ss <jabwunza@gmail.com>
2024-01-16 20:50:07 +0300

    pi, min and max

---
+++ math.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+// PHP has a set of math functions that allows you to perform mathematical tasks on numbers.
+
+echo(pi());     // returns the value of pi
+
+// php min and max
+
+echo(min(0, 150, 30, 20, -8, -200));
+echo(max(0, 150, 30, 20, -8, -200));
+
+
+?>
+</body>
+</html>
