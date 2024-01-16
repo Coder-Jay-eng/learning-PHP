55291975ee7a0dbdef2ff7a86092f2cc2a511466
Aw3som3ss <jabwunza@gmail.com>
2024-01-16 20:15:19 +0300

    casting to boolean

--- casting.php
+++ casting.php
@@ -60,6 +60,34 @@ $u = (int) $u;
 
 echo "$z";
 
+
+// Cast to boolean
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = 0;       // Integer
+$d = -1;      // Integer
+$e = 0.1;     // Float
+$f = "hello"; // String
+$g = "";      // String
+$h = true;    // Boolean
+$i = NULL;    // NULL
+
+$a = (bool) $a;
+$b = (bool) $b;
+$c = (bool) $c;
+$d = (bool) $d;
+$e = (bool) $e;
+$f = (bool) $f;
+$g = (bool) $g;
+$h = (bool) $h;
+$i = (bool) $i;
+
+/*
+If a value is 0, NULL, false, or empty, the (bool) converts it into false, otherwise true.
+
+Even -1 converts to true.
+*/
     ?>
 </body>
 </html>
